a49a1b02623591eb4261747d48cf527b98499d66
santosst3 <73975120+santosst3@users.noreply.github.com>
2022-10-27 13:00:39 -0300

    Modifying optimization for conventional ISMC

--- run_simulink.m
+++ run_simulink.m
@@ -59,10 +59,11 @@ iq_ref = 0;
 
 ganhoruido = 5;
 tipo_filtro = 6;
-tipo_rejharm = 2;
+tipo_rejharm = 1;
 
 %% Lê arquivo referente a partícula
 smc_param
+tr_H = tr_H * 100; % Para tipo_rejharm == 1
 
 %% Roda simulação
 out = sim('simulacao.slx');
